605b817143c32734ee16562c73e6d6625ec549b0
John Brunton <john_brunton@live.co.uk>
2022-04-29 22:37:24 +0100

    red: singleton list, positive

--- closest.spec.js
+++ closest.spec.js
@@ -3,3 +3,7 @@ const closest = require("./closest");
 test("returns null for empty array", () => {
   expect(closest([])).toEqual(null);
 });
+
+test("singleton list, positive", () => {
+  expect(closest([1])).toEqual(1);
+});
