7e831a343d3a842388d04454f59a3be868f6e7a7
John Brunton <john_brunton@live.co.uk>
2022-04-30 16:30:18 +0100

    red: returns null for empty array

--- src/closest.js
+++ src/closest.js
@@ -1,22 +1,3 @@
-const closerToZero = (x, y) => {
-  const sizeX = Math.abs(x);
-  const sizeY = Math.abs(y);
-  return sizeX < sizeY || (sizeX === sizeY && x > 0);
-};
-
-const closest = (xs) => {
-  if (xs.length === 0) {
-    return null;
-  }
-
-  var closest = xs[0];
-  for (const candidate of xs.slice(1)) {
-    if (closerToZero(candidate, closest)) {
-      closest = candidate;
-    }
-  }
-
-  return closest;
-};
+const closest = () => {};
 
 module.exports = closest;
