5e7dcc5f30587939f6715c7488de58f65ddf8c84
John Brunton <john_brunton@live.co.uk>
2022-04-30 16:31:21 +0100

    red: singleton array [0]

--- src/closest.spec.js
+++ src/closest.spec.js
@@ -3,3 +3,7 @@ const closest = require("./closest");
 test("returns null for empty array", () => {
   expect(closest([])).toEqual(null);
 });
+
+test("singleton array [0]", () => {
+  expect(closest([0])).toEqual(0);
+});
