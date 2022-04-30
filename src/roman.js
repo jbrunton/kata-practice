bd41df1fc6e61820c8ce5a14330de4af4105bec4
John Brunton <john_brunton@live.co.uk>
2022-04-30 15:49:36 +0100

    green: converts 11

--- src/roman.js
+++ src/roman.js
@@ -1,5 +1,5 @@
 const roman = (input) => {
-  if (input === 10) return "X";
+  if (input >= 10) return "X" + roman(input - 10);
   if (input >= 5) return "V" + roman(input - 5);
   if (input >= 1) return "I" + roman(input - 1);
   return "";
