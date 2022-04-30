974f810cf1dab732cd17469825378883fdb85d54
John Brunton <john_brunton@live.co.uk>
2022-04-30 15:46:41 +0100

    red: converts 5

--- src/roman.spec.js
+++ src/roman.spec.js
@@ -5,6 +5,7 @@ const roman = require("./roman");
   [1, "I"],
   [2, "II"],
   [3, "III"],
+  [5, "V"],
 ].forEach(([input, expected]) => {
   test(`converts ${input}`, () => {
     expect(roman(input)).toEqual(expected);
