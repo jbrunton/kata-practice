53323be4c864a5e6d5e76921c6eaf27351917dc8
John Brunton <john_brunton@live.co.uk>
2022-07-26 21:17:53 +0100

    green: two items, asc

--- src/closest.js
+++ src/closest.js
@@ -1,7 +1,9 @@
 const closest = (xs) => {
   if (xs.length === 0) return null;
   if (xs.length === 2) {
-    return xs[1];
+    if (xs[0] > xs[1]) {
+      return xs[1];
+    }
   }
   return xs[0];
 };
