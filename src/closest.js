cd15b76668288debe4431698eac44354c8b3067b
John Brunton <john_brunton@live.co.uk>
2022-07-26 21:25:54 +0100

    refactor: extract isCloser

--- src/closest.js
+++ src/closest.js
@@ -3,14 +3,18 @@ const closest = (xs) => {
   let closest = xs[0];
   if (xs.length === 2) {
     let candidate = xs[1];
-    if (
-      Math.abs(candidate) < Math.abs(closest) ||
-      (Math.abs(candidate) === Math.abs(closest) && candidate > 0)
-    ) {
+    if (isCloser(candidate, closest)) {
       closest = candidate;
     }
   }
   return closest;
+};
+
+const isCloser = (candidate, closest) => {
+  return (
+    Math.abs(candidate) < Math.abs(closest) ||
+    (Math.abs(candidate) === Math.abs(closest) && candidate > 0)
+  );
 };
 
 module.exports = closest;
