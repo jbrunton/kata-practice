e0453768ae4db580fe7fe0d577b8ce237146d3ef
John Brunton <john_brunton@live.co.uk>
2022-07-26 21:08:30 +0100

    reset closest

--- src/closest.js
+++ src/closest.js
@@ -1,25 +1,3 @@
-const closest = (xs) => {
-  if (xs.length === 0) {
-    return null;
-  }
-
-  var closest = xs[0];
-  for (const candidate of xs.slice(1)) {
-    if (closerToZero(candidate, closest)) {
-      closest = candidate;
-    }
-  }
-
-  return closest;
-};
-
-const closerToZero = (candidate, closest) => {
-  const candidateSize = Math.abs(candidate);
-  const closestSize = Math.abs(closest);
-  return (
-    candidateSize < closestSize ||
-    (candidateSize === closestSize && candidate >= 0)
-  );
-};
+const closest = () => {};
 
 module.exports = closest;
