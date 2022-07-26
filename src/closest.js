f399096c4bddd8416fd2508da91c5399e7a8fa2b
John Brunton <john_brunton@live.co.uk>
2022-07-26 21:24:36 +0100

    green: two items, same size, different signs

--- src/closest.js
+++ src/closest.js
@@ -3,7 +3,10 @@ const closest = (xs) => {
   let closest = xs[0];
   if (xs.length === 2) {
     let candidate = xs[1];
-    if (Math.abs(candidate) < Math.abs(closest)) {
+    if (
+      Math.abs(candidate) < Math.abs(closest) ||
+      (Math.abs(candidate) === Math.abs(closest) && candidate > 0)
+    ) {
       closest = candidate;
     }
   }
